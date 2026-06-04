766359acf79fbfa519e1b2e450996fb7504d61d2
Laryssa Silva <laryssasiilvafs@gmail.com>
2026-06-04 19:49:12 -0300

    feat: add vehicle type selection menu

--- src/main/java/com/laryssa/fipeexplorer/FipeexplorerApplication.java
+++ src/main/java/com/laryssa/fipeexplorer/FipeexplorerApplication.java
@@ -1,5 +1,6 @@
 package com.laryssa.fipeexplorer;
 
+import com.laryssa.fipeexplorer.main.Main;
 import com.laryssa.fipeexplorer.service.ConsumoAPI;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,10 +15,8 @@ public class FipeexplorerApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoAPI = new ConsumoAPI();
-
-		consumoAPI.obterDados("https://parallelum.com.br/fipe/api/v1/carros/marcas");
-		System.out.println(consumoAPI);
+		var main = new Main();
+		main.exibeMenu();
 
 	}
 
---
+++ src/main/java/com/laryssa/fipeexplorer/main/Main.java
@@ -0,0 +1,38 @@
+package com.laryssa.fipeexplorer.main;
+
+import com.laryssa.fipeexplorer.service.ConsumoAPI;
+
+import java.util.Scanner;
+
+public class Main {
+    private Scanner leitura = new Scanner(System.in);
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1/";
+    private ConsumoAPI consumo = new ConsumoAPI();
+
+    public void exibeMenu() {
+        var menu = """
+                *** OPÇÕES ***
+                Carro
+                Moto
+                Caminhão
+                
+                Digite uma das opções para consultar:
+                """;
+
+        System.out.println(menu);
+        var opcao = leitura.nextLine();
+
+        String endereco;
+
+        if (opcao.toLowerCase().contains("carr")) {
+            endereco = URL_BASE + "carros/marcas";
+        } else if (opcao.toLowerCase().contains("mot")) {
+            endereco = URL_BASE + "motos/marcas";
+        } else {
+            endereco = URL_BASE + "caminhoes/marcas";
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
+    }
+}
