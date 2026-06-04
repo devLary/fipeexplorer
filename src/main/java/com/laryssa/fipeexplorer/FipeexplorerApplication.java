27af3cc7e3ac36c5bd904218e869e29e55b2e1ed
Laryssa Silva <laryssasiilvafs@gmail.com>
2026-06-04 18:35:39 -0300

    Initial commit

---
+++ src/main/java/com/laryssa/fipeexplorer/FipeexplorerApplication.java
@@ -0,0 +1,13 @@
+package com.laryssa.fipeexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FipeexplorerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FipeexplorerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laryssa/fipeexplorer/FipeexplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laryssa.fipeexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FipeexplorerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
